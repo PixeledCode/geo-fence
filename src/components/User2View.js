35f8196b13b67cd12aacc27797ac80b8f1a7c4aa
ShoaibIND <hello@shoaibdev.me>
2020-12-19 20:14:35 +0530

    bug fixes

--- src/components/User2View.js
+++ src/components/User2View.js
@@ -3,7 +3,8 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import {LocationIcon} from './LocationIcon';
 import Distance from './Distance'
-var dist;
+var distance;
+var lat1, lon1, lat2, lon2;
 const MyMarker = props => {
 
   const initMarker = ref => {
@@ -22,6 +23,8 @@ class User2View extends Component {
       currentPos2: null};
     this.state = {
       range: null};
+    this.state = {
+      dist: null};
     this.handleClick = this.handleClick.bind(this);   
   }
 
@@ -29,25 +32,43 @@ class User2View extends Component {
     this.setState({ currentPos2: e.latlng });
     sessionStorage.setItem('Poslat2', this.state.currentPos2.lat);
     sessionStorage.setItem('Poslon2', this.state.currentPos2.lng);
-    const lat1 = sessionStorage.getItem('Poslat1');
-    const lon1 = sessionStorage.getItem('Poslon1');
-    const lat2 = sessionStorage.getItem('Poslat2');
-    const lon2 = sessionStorage.getItem('Poslon2');
-    dist = Distance(lat1, lon1, lat2, lon2);
-    console.log(dist);
-    if (dist <= 1) {
+    // lat1 = sessionStorage.getItem('Poslat1');
+    // lon1 = sessionStorage.getItem('Poslon1');
+    lat2 = sessionStorage.getItem('Poslat2');
+    lon2 = sessionStorage.getItem('Poslon2');
+    this.setState({ dist: Distance(lat1, lon1, lat2, lon2) })
+    sessionStorage.setItem('dist1', this.state.dist);
+    console.log(this.state.dist);
+    if (this.state.dist <= 1) {
       this.setState({ range : "User is in range" });
     }
     else {
       this.setState({ range : "User is not in range" });
     }
+    if (!(lat1 && lon1)) {
+        this.setState({ range : "User 1 location unavailable" });
+      }
   }
 
   componentDidMount() {
-    const lat2 = sessionStorage.getItem("Poslat2");
-    const lon2 = sessionStorage.getItem("Poslon2");
+    lat1 = sessionStorage.getItem('Poslat1');
+    lon1 = sessionStorage.getItem('Poslon1');
+    lat2 = sessionStorage.getItem("Poslat2");
+    lon2 = sessionStorage.getItem("Poslon2");
+    distance = Distance(lat1, lon1, lat2, lon2)
     if (lat2 && lon2) this.setState({ currentPos2: [lat2, lon2] });
-    if (!(this.state.range)) this.setState({range: 'Click on location'});
+    console.log(distance);
+    if (lat2 && lon2) {
+      if (distance <= 1) {
+        this.setState({ range : "User is in range" });
+      }
+      else {
+        this.setState({ range : "User is not in range" });
+      }
+      if (!(lat1 && lon1)) {
+        this.setState({ range : "User 1 location unavailable" });
+      }
+    }
   }
 
   render() {
