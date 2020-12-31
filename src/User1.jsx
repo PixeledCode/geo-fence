be8186efcc3dfcd917831d0793609c9da4215dd9
ShoaibIND <hello@shoaibdev.me>
2020-12-31 17:55:40 +0530

    eslint fixed

--- src/User1.jsx
+++ src/User1.jsx
@@ -5,7 +5,7 @@ import './App.css';
 function App1() {
   return (
     <div className="App1">
-      <MapView zoom={15} center={{ lat: 32.74197573175059, lng: 74.86066465077062 }}/>
+      <MapView zoom={15} center={{ lat: 32.74197573175059, lng: 74.86066465077062 }} />
     </div>
   );
 }
